31301cbec18d280032244eb8e0e695e89e28b21f
Nouaamane <noro6@gmail.com>
2023-08-30 01:05:14 +0100

    ll

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,86 +1,63 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+# Author - MoOka
+"""class Square that defines a square"""
 
 
 class Square:
-    """
-    A class representing a square.
-
-    Attributes:
-        __size (int): the size of the square (private)
-    """
+    """class Square that defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """
-        Initializes a new instance of the Square class.
-
-        Parameters:
-            size (int): the size of the square (default 0)
-        """
-        self.size = size
-        self.position = position
+        """Instantiation with optional size and position"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif not type(position) is tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not type(position[0]) is int) or (not type(position[1]) is int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__size = size
+            self.__position = position
 
     @property
     def size(self):
-        """
-        Gets the value of the size attribute.
-
-        Returns:
-            int: the size of the square
-        """
+        """property to retrieve it"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Sets the value of the size attribute.
-
-        Parameters:
-            value (int): the new size of the square
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+        """property setter to set it"""
         self.__size = value
 
     @property
     def position(self):
-        """gets the pos attribute value
-        Returns:
-            tuple: square's position
-        """
+        """property to retrieve it"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """sets the value of positon attribute.
-        args:
-            valut(tuple): pos of the square
-        """
-        if (not isinstance(value, tuple) or len(value) != 2 or
-                not all(isinstance(x, int) and x >= 0 for x in value)):
-            raise TypeError('position must be a tuple of 2 positive integers')
+        """property setter to set it"""
         self.__position = value
 
     def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-            int: the area of the square
-        """
-        return self.__size ** 2
+        """Public instance that returns the  square area"""
+        return self.__size * self.__size
 
     def my_print(self):
-        """prints the square using '#' and the given position. """
+        """Public instance that prints the square with the char #"""
         if self.__size == 0:
-            print()
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            print(' ' * self.__position[0] + '#' * self.__size)
+            print("")
+            return
+        if self.__position[1] > 0:
+            for l in range(0, self.__position[1]):
+                print("")
+        for i in range(0, self.__size):
+            if self.__position[0] > 0:
+                for k in range(0, self.__position[0]):
+                    print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
